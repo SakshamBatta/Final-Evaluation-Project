324ec4899ca5bb083645119a1ff7d593ec3350a6
Saksham Batta <sakshambatta20@gmail.com>
2025-04-29 19:01:35 +0530

    Changed home page route

--- Hubly/frontend/src/App.jsx
+++ Hubly/frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <ChatbotProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
 
